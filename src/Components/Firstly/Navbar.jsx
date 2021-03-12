315f9ad5e91e2f36b58c6b5101ddbfda9441b299
shahzadrori <shahzadrori@gmail.com>
2021-03-12 11:21:26 +0500

    introducing the navabar

---
+++ src/Components/Firstly/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Nav = ()=>{
+    return(
+        <>
+            <div className='navs'>
+                <ul>
+                    <li>Home</li>
+                    <li>About us</li>
+                    <li>Contact us</li>
+                </ul>
+            </div>
+        </>
+    )
+}
+
+export default Nav
