755c8ea59526e6f943b8fffed73d1746d8116be4
shahzadrori <shahzadrori@gmail.com>
2021-03-13 14:02:36 +0500

    introducing components in redux

---
+++ src/Redux/Actinon.jsx
@@ -0,0 +1,8 @@
+import { ADD } from "./Type"
+
+export const Add_It=(Data)=>{
+    return{
+        type:ADD,
+        payload:Data
+    }
+}
---
+++ src/Redux/State.jsx
@@ -0,0 +1,3 @@
+export const Init_State={
+    card_item:[]
+}
