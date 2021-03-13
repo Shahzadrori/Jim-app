a075764e3638bf3547aeabc9c17fbf830017db0f
shahzadrori <shahzadrori@gmail.com>
2021-03-13 14:55:34 +0500

    introducing reducer

--- src/Redux/Reducers.jsx
+++ src/Redux/Reducers.jsx
@@ -1,3 +1,15 @@
 import React from 'react';
+import { Add_It } from './Actinon';
+import { Init_State } from './State';
 
-export const Reducer=(state)=>{}
+export const Reducer=(state = Init_State,action)=>{
+    switch(action.type){
+        case Add_It:
+            return{
+                ...state,
+                Persona:[...state,action.payload]
+            }
+            default:
+            return state
+    }
+}
