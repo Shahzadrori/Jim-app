62a5193849f6e7b3e276620faf6eec6de4009957
shahzadrori <shahzadrori@gmail.com>
2021-03-13 10:31:03 +0500

    update

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
           <Route exact path='/' component={Home}/>
           <Route exact path='/about' component={About}/>
           <Route exact path='/contact' component={Contact}/>
-          {/* <Route exact path='/login' component={Form}/> */}
+          <Route exact path='/login' component={Form}/>
           <Route component={Error_msg}/>
       </Switch>
         </>
