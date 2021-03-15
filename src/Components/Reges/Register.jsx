3c0db85b4fbdc3825380c4817538285bf6d9287d
shahzadrori <shahzadrori@gmail.com>
2021-03-15 13:07:46 +0500

    styling the regis

--- src/Components/Reges/Register.jsx
+++ src/Components/Reges/Register.jsx
@@ -6,10 +6,30 @@ import "react-toastify/dist/ReactToastify.css";
 const Regis_form = () => {
   function toasts() {
     toast.success("Jobs Done");
+    toast.dark('New Registration', {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        });
   }
 
   return (
     <>
+           <ToastContainer
+          position="top-right"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+        />
       <div className="main_regis">
         <div className="inner_regis">
           <h1>Login</h1>
@@ -21,19 +41,8 @@ const Regis_form = () => {
           <input type="Number" placeholder="Enter Your Phone Number" />
           <label>Age :</label>
           <input type="text" placeholder="Enter Your Age in Numbers" />
+          <button onClick={toasts}>Submit</button>
         </div>
-        <ToastContainer
-          position="top-right"
-          autoClose={5000}
-          hideProgressBar={false}
-          newestOnTop={false}
-          closeOnClick
-          rtl={false}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
-        />
-        <button onClick={toasts}>Submit</button>
       </div>
     </>
   );
