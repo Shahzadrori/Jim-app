7129e49557e5669fe6157e93855541c8741cf757
shahzadrori <shahzadrori@gmail.com>
2021-03-15 11:56:24 +0500

    styling the regis component

--- src/Components/Reges/Register.jsx
+++ src/Components/Reges/Register.jsx
@@ -1,23 +1,27 @@
-import React from 'react';
-import '../../style/Regis.css'
+import React from "react";
+import { ToastContainer } from "react-toastify";
+import "../../style/Regis.css";
 
-const Regis_form = ()=>{
-      return(
-          <>
-              <div className='main_regis'>
-              <div className='inner_regis'>
-                  <label>Name :</label>
-                  <input type='text' placeholder='Enter Your Name'/>
-                  <label>ID :</label>
-                  <input type='Number' placeholder='Enter Your ID Card Number'/>
-                  <label>Phone :</label>
-                  <input type='Number' placeholder='Enter Your Phone Numbert'/>
-                  <label>Age :</label>
-                  <input type='text' placeholder='Enter Your Name'/>
-                  </div>
-              </div>
-          </>
-      )
-}
+const Regis_form = () => {
+  return (
+    <>
+      <div className="main_regis">
+        <div className="inner_regis">
+          <h1>Login</h1>
+          <label>Name :</label>
+          <input type="text" placeholder="Enter Your Name" />
+          <label>ID :</label>
+          <input type="Number" placeholder="Enter Your ID Card Number" />
+          <label>Phone :</label>
+          <input type="Number" placeholder="Enter Your Phone Numbert" />
+          <label>Age :</label>
+          <input type="text" placeholder="Enter Your Age in Numbers" />
+        </div>
+
+         
+      </div>
+    </>
+  );
+};
 
 export default Regis_form;
