6a05028a409da97edb5a30ec0fdcda1ef789461e
shahzadrori <shahzadrori@gmail.com>
2021-03-23 11:14:46 +0500

    using idb to store data

--- src/Components/DB/Db.jsx
+++ src/Components/DB/Db.jsx
@@ -1,94 +1,25 @@
-import Dexie from 'dexie';
-import { connect } from 'react-redux';
-import { Take_It } from '../../Redux/Actinon';
-
-const  Database = async (regis_value)=>{
-    // console.log(regis_value)
-//   async  function Dataphase(){
-    const db = new Dexie('Database');
-   await db.version(1).stores({notes: '++id'})
-   await db.open()
-   try{
-     await db.notes.add(regis_value);
-   }catch (error){
-       alert('Same id exists' + error)
-   }
-// return    console.log(  await db.notes.each((res=> console.log(res))))
-
-// if (!window.indexedDB) {
-//     console.log("Your browser doesn't support a stable version of IndexedDB. Such and such feature will not be available.");
-// }
-// // var request = window.indexedDB.open("MyTestDatabase", 3);
-// // request.onerror = function(event) {
-// //     // Do something with request.errorCode!
-// //     alert(event)
-// //   };
-// //   request.onsuccess = function(event) {
-// //     // Do something with request.result!
-// //     alert(event + 'success')
-// //   };
-// // var db;
-// // var request = indexedDB.open("MyTestDatabase");
-// // request.onerror = function(event) {
-// //   console.log("Why didn't you allow my web app to use IndexedDB?!");
-// // };
-// // request.onsuccess = function(event) {
-// //   db = event.target.result;
-// // };
-// const customerData = [
-//     { ssn: "444-44-4444", name: "Bill", age: 35, email: "bill@company.com" },
-//     { ssn: "555-55-5555", name: "Donna", age: 32, email: "donna@home.org" }
-//   ];
-
-// const dbName = "the_name";
-
-// var request = indexedDB.open(dbName, 1);
-
-// request.onerror = function(event) {
-//   // Handle errors.
-// };
-// request.onupgradeneeded = function(event,index) {
-//   var db = event.target.result;
-
-//   // Create an objectStore to hold information about our customers. We're
-//   // going to use "ssn" as our key path because it's guaranteed to be
-//   // unique - or at least that's what I was told during the kickoff meeting.
-//   var objectStore = db.createObjectStore("customers", { keyPath: "ssn" });
-
-//   // Create an index to search customers by name. We may have duplicates
-//   // so we can't use a unique index.
-//   objectStore.createIndex("name", "name", { unique: false });
-
-//   // Create an index to search customers by email. We want to ensure that
-//   // no two customers have the same email, so use a unique index.
-//   objectStore.createIndex("email", index, { unique: true });
-
-//   // Use transaction oncomplete to make sure the objectStore creation is
-//   // finished before adding data into it.
-//   objectStore.transaction.oncomplete = function(event) {
-//     // Store values in the newly created objectStore.
-//     var customerObjectStore = db.transaction("customers", "readwrite").objectStore("customers");
-//     customerData.forEach(function(customer) {
-//       customerObjectStore.add(customer);
-//     });
-//   };
-// };
+import { openDB } from "idb"
 
 
 
-
-
-}
-// }
-//  const mapstatess = (state)=>{
-//      console.log(state)
-//  }
-//  const mapdispatches = (dispatche)=>{
-//      return{
-//          add_that:()=>{
-//              dispatche(Take_It())
-//          }
-//             }
-//         }
-//    export default connect(mapstatess,mapdispatches)(Database);
-export default  Database
+export async function Database(regis,index) {
+    console.log(regis)
+    console.log(regis.id)
+    const db1 = await
+    openDB('db', 1, {
+        upgrade(db) {
+          db.createObjectStore('store1',{
+              keyPath: 'key',
+            autoIncrement:true
+            });
+        },
+      });
+    await db1.put('store1',{key: Number(regis.id),value: regis})
+      .then(result => {
+        console.log('success!', result);
+      })
+      .catch(err => {
+        console.error('error: ', err);
+      });
+    db1.close();
+  }
--- src/Components/Reges/Register.jsx
+++ src/Components/Reges/Register.jsx
@@ -6,8 +6,7 @@ import { openDB } from "idb";
 import { connect } from "react-redux";
 import { Take_It } from "../../Redux/Actinon";
 import Dexie from 'dexie'
-// import Database from '../DB/Db'
-// import Dataget from '../DB/Notes'
+import {Database} from "../DB/Db";
 const Regis_form = (pare) => {
   const [inp_val, setinp_val] = useState({
     name: "",
@@ -15,20 +14,7 @@ const Regis_form = (pare) => {
     phone: "",
     age: "",
   });
-//   const  Database = async ()=>{
-//     // console.log(regis_value)
-// //   async  function Dataphase(){
-//     const db = new Dexie('Database');
-//    await db.version(1).stores({notes: '++id'})
-//    await db.open()
-//    try{
-//      await db.notes.add(pare.values);
-//      await db.notes.each((result)=>{
-//        pare.take_it(result)
-//      })
-//    }catch (error){
-//        alert('Same id exists' + error)
-//    }}
+
    function toasts() {
     toast.dark(`Jobs Done`, {
       position: "top-right",
@@ -40,35 +26,7 @@ const Regis_form = (pare) => {
       progress: undefined,
     });
   }
-  async function Database(){
-    const db = new Dexie('Database');
-   await db.version(1).stores({notes: '++id'})
-   await db.open()
-   try{
-     await db.notes.add(inp_val)
-     await db.notes.each(info => pare.take_it(info))
-   }catch(error){
-     alert(error)
-   }
-   }
 
-  //  Database()
-  // async function doDatabaseStuff() {
-  //   const db = await openDB(`Data`, 1, {
-  //     upgrade(db) {
-  //       let storeRow = db.createObjectStore("rowData", {
-  //         keyPath: "key",
-  //         autoIncrement: true,
-  //       });
-  //     },
-  //   });
-  //   try {
-  //     await db.put("rowData", { key: Number(inp_val.id), value: inp_val });
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  //   db.close();
-  // }
 
   const Submit = async () => {
     var names = document.getElementById("names").value;
@@ -78,32 +36,19 @@ const Regis_form = (pare) => {
 
     if (names === "" || names === null) {
       toast.error("Name field should not be empty");
-    } else if (names.length === 4 || names.length < 4) {
-      toast.error("Name field should atleast contain 4 digits");
-    } else if (Number(names)) {
-      toast.error("Name shoul contain alphabet letters");
-    } else if (id.length !== 13) {
-      toast.error("ID Number should be equal to 13");
-    } else if (phone.length !== 11) {
-      toast.error("Phone Number should contain 11 digits");
-    } else if (age < 16) {
-      toast.error("Age should be above sixteen");
+    // } else if (names.length === 4 || names.length < 4) {
+    //   toast.error("Name field should atleast contain 4 digits");
+    // } else if (Number(names)) {
+    //   toast.error("Name shoul contain alphabet letters");
+    // } else if (id.length !== 13) {
+    //   toast.error("ID Number should be equal to 13");
+    // } else if (phone.length !== 11) {
+    //   toast.error("Phone Number should contain 11 digits");
+    // } else if (age < 16) {
+    //   toast.error("Age should be above sixteen");
     } else {
-      // Database(inp_val);
-      // Database(inp_val)
-      Database()
-      toasts();
-      // doDatabaseStuff();
-    //  Database();
-      // toast.success("Jobs Done");
-      // const storage = localStorage.setItem("formdata", JSON.stringify(pare.values));
-      // function Dispatchdata() {
-      // localStorage.getItem("formdata")
-      //     const { Name,ID , Phone, Age } = JSON.parse(
-      //       localStorage.getItem("formdata")
-      //     );
-      //     console.log(`${Name} and ${ID} and ${Phone} and ${Age}`);
-      // }
+      console.log('Done')
+      Database(inp_val)
 
     }
   };
